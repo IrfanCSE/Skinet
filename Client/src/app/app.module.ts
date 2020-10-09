2c1c435f22e1d40c15bb64cd851c4066ab74d4fc
Irfan <irfanulhasan1996@gmail.com>
2020-10-09 20:12:40 +0600

    Added loading for delay request

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ErrorInterceptor } from './core/intercept/error-interceptor.interceptor';
+import { BusyInterceptor } from './core/intercept/busy.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,10 +19,12 @@ import { ErrorInterceptor } from './core/intercept/error-interceptor.interceptor
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    RouterModule
+    RouterModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: BusyInterceptor, multi: true}
   ],
   bootstrap: [AppComponent],
 })
---
+++ Client/src/app/core/intercept/busy.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { delay, finalize } from 'rxjs/operators';
+
+@Injectable()
+export class BusyInterceptor implements HttpInterceptor {
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.spinner.show();
+
+    return next.handle(request).pipe(
+      delay(1000),
+      finalize(() => {
+        this.spinner.hide();
+      })
+    );
+  }
+}
