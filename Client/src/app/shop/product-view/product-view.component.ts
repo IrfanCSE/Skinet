128a327408a8482e1e02616946acb88c5dfc88aa
Irfan <irfanulhasan1996@gmail.com>
2020-10-23 19:11:17 +0600

    Basket In-Dec functionality

--- Client/src/app/shop/product-view/product-view.component.ts
+++ Client/src/app/shop/product-view/product-view.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from 'src/app/basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/Models/product';
@@ -7,13 +8,18 @@ import { ShopService } from '../shop.service';
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.scss']
+  styleUrls: ['./product-view.component.scss'],
 })
 export class ProductViewComponent implements OnInit {
-
   product: Product;
+  quantity = 1;
 
-  constructor(private shopService: ShopService, private param: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(
+    private shopService: ShopService,
+    private param: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
     this.bcService.set('@productName', ' ');
   }
 
@@ -22,18 +28,33 @@ export class ProductViewComponent implements OnInit {
     this.getProduct(id);
   }
 
-  getProduct(id: number){
-    this.shopService.getProduct(id).subscribe(res => {
-      this.product = res;
-      this.bcService.set('@productName', this.product.name);
-    },
-    error => {
-      console.log(error);
-    });
+  getProduct(id: number) {
+    this.shopService.getProduct(id).subscribe(
+      (res) => {
+        this.product = res;
+        this.bcService.set('@productName', this.product.name);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  getProductId(){
+  getProductId() {
     return +this.param.snapshot.paramMap.get('id');
   }
 
+  quantityIncrement() {
+    this.quantity++;
+  }
+
+  quantityDecrement() {
+    if (this.quantity > 1){
+      this.quantity--;
+    }
+  }
+
+  AddItemToBasket(){
+    this.basketService.setItemInBasket(this.product, this.quantity);
+  }
 }
